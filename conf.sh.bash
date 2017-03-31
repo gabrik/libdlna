a2fcc9f01608aefd6ccb25beab7dee33f2c237c6
Gabriele Baldoni <>
2017-03-31 11:00:55 +0200

    Update libav references

---
+++ conf.sh.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 
+# File:   conf.sh.bash
+# Author: gabriele
+#
+# Created on 30-mar-2017, 16.10.01
+#
+./configure --enable-developer --enable-debug --disable-strip --disable-optimize 
